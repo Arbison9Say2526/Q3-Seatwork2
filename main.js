50ff2737f6fd9ae1eb49108f70b23a0b3f716055
Arbison9Say2526 <arbison.ab@obmontessori.edu.ph>
2026-03-03 08:00:50 +0800

    Refactor discount calculation and HTML updates

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 function contemplateDiscount(){
-    let budget = document.getElementById("budget").value
-    let discounted = budget * .2;
+    let budget = Number(document.getElementById("budget").value);
+    let discounted = Number(budget * .2).toFixed(2);
 
-    document.getElementById("discount").innerHTML = '<div class="container p-5 bg-light text-dark"> <h5>Recommended Brands:</h5> <div class="text-center mt-3"> <button type="button" onclick="window.location.href='https://www.epson.com.ph'">EPSON</button> <button type="button" onclick="window.location.href='https://www.brother.com.ph/en'">Brother</button> <button type="button" onclick="window.location.href='https://ph.canon/en/consumer'">Canon</button> <button type="button" onclick="window.location.href='https://www.hp.com/ph-en/home.html'">HP</button> <h5>Eligible discount:</h5> <button type="button" class=" btn btn-primary">6,700</button> </div>
-    ';
+    document.getElementById("discounted").innerHTML = `
+            <div class="container p-5 bg-light text-dark">
+            <h5>Recommended Brands:</h5>
+            <div class="text-center mt-3">
+            <button type="button" onclick="window.location.href='https://www.epson.com.ph/'">EPSON</button>
+            <button type="button" onclick="window.location.href='https://www.brother.com.ph/en'">Brother</button> 
+            <button type="button" onclick="window.location.href='https://ph.canon/en/consumer'">Canon</button> 
+            <button type="button" onclick="window.location.href='https://www.hp.com/ph-en/home.html'">HP</button> 
+            <h5>Eligible discount:</h5> <button type="button" class=" btn btn-primary">${discounted}</button>
+        </div>`;
 }
 
 function seeLocations(){
-    document.getElementById("discount").innerHTML = '<div class="container p-5 bg-light text-dark"> <h2>Branches</h2> <p>OBMC Eisenhower Street, Greenhills, San Juan</p> <p>Happyland, Tondo, Manila</p> </div>';
+
+  
+    document.getElementById("seeLocations").innerHTML = `
+    <div class="container p-5 bg-light text-dark">
+     <h2>Branches</h2> 
+     <p>OBMC Eisenhower Street, Greenhills, San Juan</p>
+      <p>Happyland, Tondo, Manila</p> </div>';`
     
 }
+
+document.getElementById(discounted)
